60c78ea748f2072ae02f11a72cd68aa73b1eb562
Hirofumi Seiyama <hseiyama@gmail.com>
2023-07-30 17:01:45 +0900

    PIC18F26K22用のサンプルコードを変更（ADCポーリング編）

--- mplabx/pic18f26k22.X/main.c
+++ mplabx/pic18f26k22.X/main.c
@@ -79,24 +79,52 @@
 #define TMR0L_INIT  0xDB        // ([0xFFFF-62,500]→1秒)
 
 volatile uint8_t data_uart;
+volatile uint16_t data_adc;
 
-// USART送信処理
-static void send_uart(uint8_t data) {
+/* USART送信 (1文字) */
+static void send_char(uint8_t data) {
     while(TX1IF == 0);          // 送信可能になるまで待つ
     TXREG1 = data;              // データを送信する
+}
+
+/* USART送信 (文字列) */
+static void send_strg(uint8_t *data) {
+    // 文字がNULLになるまで継続
+    while(*data != 0x00) {
+        send_char(*data);       // USART送信 (1文字)
+        data++;
+    }
+}
+
+/* HEX数字送信 */
+static void echo_hex(uint8_t hex_data) {
+    uint8_t hex_table[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+    };
+    send_char(hex_table[(hex_data >> 4) & 0x0F]);
+    send_char(hex_table[hex_data & 0x0F]);
+}
+
+/* 16進数データ送信 (2byte) */
+static void echo_2byte(uint16_t data) {
+    uint8_t msg[] = "0x";
+    send_strg(msg);
+    echo_hex((data >> 8) & 0xFF);   // HEX数字送信
+    echo_hex(data & 0xFF);          // HEX数字送信
 }
 
 // 受信割込み処理
 void __interrupt() isr(void) {
     if (RC1IF == 1) {           // 割込みはUSART通信の受信か？
         data_uart = RCREG1;     // レジスタからデータを受信
-        send_uart(data_uart);   // データ受信を送信する
+        send_char(data_uart);   // データ受信を送信する
     }
     if (TMR0IF == 1) {          // タイマー0の割込み発生か？
         TMR0H = TMR0H_INIT;     // タイマー0H,Lのカウント開始の初期値をセットする
         TMR0L = TMR0L_INIT;     // (1秒)
         /* 1msタイマ割り込み毎の処理 */
         {
+            /* PORT操作処理 */
             LATA0 = !LATA0;         // 2番ピン(RA0)を反転出力する
             LATA1 = !PORTBbits.RB0; // RB0を反転し、RA1に出力する
             LATA2 = PORTBbits.RB1;  // RB1を入力し、RA1に出力する
@@ -121,7 +149,7 @@ void main(void) {
     PLLEN   = 1;                // PLL(x4)を有効化(→64MHz)
     /* PORT初期化 */
     ANSELA  = 0b00000000;       // AN0-4 アナログは使用しない、デジタルI/Oに割当
-    ANSELB  = 0b00000000;       // AN8-13アナログは使用しない、デジタルI/Oに割当
+    ANSELB  = 0b00000100;       // AN8ｱﾅﾛｸﾞのみ使用する、9-13ｱﾅﾛｸﾞは使用しない
     ANSELC  = 0b00000000;       // AN14-19アナログは使用しない、デジタルI/Oに割当
     TRISA   = 0b11111000;       // RA0-2のみ出力に設定、1で入力 0で出力
     TRISB   = 0b11111111;       // RB0-RB7全て入力に設定
@@ -137,17 +165,29 @@ void main(void) {
     SPBRG1  = 103;              // ボーレートを9600(低速モード)に設定
     RC1IE   = 1;                // USART割込み受信を有効にする
     /* TIMER0初期化 */
-    T0CON  = 0b10000111;        // 内部ｸﾛｯｸ(64MHz/4)でTIMER0を16ビットにて使用、ﾌﾟﾘｽｹｰﾙ値 1:256
-    TMR0H  = TMR0H_INIT;        // タイマー0H,Lのカウント開始の初期値をセットする
-    TMR0L  = TMR0L_INIT;        // (この書込みﾀｲﾐﾝｸﾞで、TMR0H,Lが反映される)
-    TMR0IF = 0;                 // タイマー0割込フラグ(T0IF)を0にする
-    TMR0IE = 1;                 // タイマー0割込み(T0IE)を許可する
+    T0CON   = 0b10000111;       // 内部ｸﾛｯｸ(64MHz/4)でTIMER0を16ビットにて使用、ﾌﾟﾘｽｹｰﾙ値 1:256
+    TMR0H   = TMR0H_INIT;       // タイマー0H,Lのカウント開始の初期値をセットする
+    TMR0L   = TMR0L_INIT;       // (この書込みﾀｲﾐﾝｸﾞで、TMR0H,Lが反映される)
+    TMR0IF  = 0;                // タイマー0割込フラグ(T0IF)を0にする
+    TMR0IE  = 1;                // タイマー0割込み(T0IE)を許可する
+    /* ADC初期化*/
+    ADCON0  = 0b00100001;       // ﾁｬﾈﾙ選択AN8、ADCを有効化
+    ADCON1  = 0b00000000;       // 負電圧VSS、正電圧VDD
+    ADCON2  = 0b10010110;       // ﾌｫｰﾏｯﾄ右詰め、ｱｸｲｼﾞｮﾝ時間4TAD、変換ｸﾛｯｸFOSC/64
     /* 全体初期化 */
     PEIE    = 1;                // 周辺装置割込みを有効にする
     ei();                       // 全割込み処理を許可する
     
     while(1) {
         wait10ms(100);          // 1秒ウエイト
-        send_uart('x');         // USART送信する
+        GO = 1;                 // A/D変換サイクルを開始する
+        while(DONE == 0);       // A/D変換が完了するまで待つ
+        data_adc = ((ADRESH << 8) | ADRESL) & 0x03FF;   // A/D結果を取得
+        /* メッセージ出力 */
+        uint8_t msg1[] = ">ADC0=";
+        uint8_t msg2[] = "\r\n";
+        send_strg(msg1);
+        echo_2byte(data_adc);   // 16進数データ送信 (2byte)
+        send_strg(msg2);
     }
 }
