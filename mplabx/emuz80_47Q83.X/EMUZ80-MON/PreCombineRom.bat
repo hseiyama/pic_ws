66b7d49032c19876729cbeca2d770f011089a909
Hirofumi Seiyama <hseiyama@gmail.com>
2023-09-02 11:51:52 +0900

    Rev.B05.5のROM(Z80)に合わせて調整

---
+++ mplabx/emuz80_47Q83.X/EMUZ80-MON/PreCombineRom.bat
@@ -0,0 +1,5 @@
+copy emuz80mon_RevB05.txt emuz80mon_RevB05_All.txt
+type add_EMUBASIC.txt >> emuz80mon_RevB05_All.txt
+type EMUZ80_ZTB.txt >> emuz80mon_RevB05_All.txt
+type GAME80.txt >> emuz80mon_RevB05_All.txt
+type vtlz80.txt >> emuz80mon_RevB05_All.txt
