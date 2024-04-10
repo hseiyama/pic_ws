3b3eb7f5e218404030eb941e26895bf56a619281
Hirofumi Seiyama <hseiyama@gmail.com>
2024-04-10 23:07:32 +0900

    basic: asm_sam07を変更(リンカーオプション追加)

--- basic/asm_sam07.X/asm_sam07.asm
+++ basic/asm_sam07.X/asm_sam07.asm
@@ -10,6 +10,7 @@ PROCESSOR 18F47Q43
 #include <xc.inc>
 
 ; ***** call graph *****************
+; Linker option "-pudata_acs=COMRAM" is needed.
 FNCONF udata_acs,?auto_,?param_				; auto=?auto_***,param=?param_***
 FNROOT main
 FNCALL main,func1
@@ -18,12 +19,12 @@ FNCALL main,func2
 ; ***** ram ************************
 PSECT udata_acs								; common memory
 count_static:
-    DS      1
+	DS		1
 
 ; ***** vector *********************
 PSECT resetVec,class=CODE,reloc=2
 resetVec:
-    goto    main
+	goto	main
 
 ; ***** func1 **********************
 PSECT udata_acs								; common memory
@@ -33,14 +34,10 @@ GLOBAL ?param_func1
 
 PSECT code
 func1:
-;	movff	?param_func1+0,?auto_func1		; param0 -> auto
-	movf	?param_func1+0,w,c
-	movwf	?auto_func1,c
+	movff	?param_func1+0,?auto_func1		; param0 -> auto
 	movlw	12h								; 12h -> w
 	addwf	?auto_func1,f,c					; w + auto -> auto
-;	movff	?auto_func1,?param_func1+1		; auto -> param1
-	movf	?auto_func1,w,c
-	movwf	?param_func1+1,c
+	movff	?auto_func1,?param_func1+1		; auto -> param1
 	return
 
 ; ***** func2 **********************
@@ -60,36 +57,26 @@ GLOBAL ?auto_main
 
 PSECT code
 main:
-    clrf	count_static,c					; 0 -> count_static
+	clrf	count_static,c					; 0 -> count_static
 	movlw	2
-    movwf	?auto_main+0,c					; 2 -> auto0
+	movwf	?auto_main+0,c					; 2 -> auto0
 	movlw	3
-    movwf	?auto_main+1,c					; 3 -> auto1
+	movwf	?auto_main+1,c					; 3 -> auto1
 loop:
 	; Call func1
-;	movff	?auto_main+0,?param_func1+0		; auto0 -> func1.param0
-	movf	?auto_main+0,w,c
-	movwf	?param_func1+0,c
+	movff	?auto_main+0,?param_func1+0		; auto0 -> func1.param0
 	call	func1
-;	movff	?param_func1+1,?auto_main+0		; func1.param1 -> auto0
-	movf	?param_func1+1,w,c
-	movwf	?auto_main+0,c
+	movff	?param_func1+1,?auto_main+0		; func1.param1 -> auto0
 	; Call func2
-;	movff	?auto_main+0,?param_func2+0		; auto0 -> func2.param0
-	movf	?auto_main+0,w,c
-	movwf	?param_func2+0,c
-;	movff	?auto_main+1,?param_func2+1		; auto1 -> func2.param1
-	movf	?auto_main+1,w,c
-	movwf	?param_func2+1,c
+	movff	?auto_main+0,?param_func2+0		; auto0 -> func2.param0
+	movff	?auto_main+1,?param_func2+1		; auto1 -> func2.param1
 	call	func2
-;	movff	?param_func2+2,?auto_main+1		; func2.param2 -> auto1
-	movf	?param_func2+2,w,c
-	movwf	?auto_main+1,c
+	movff	?param_func2+2,?auto_main+1		; func2.param2 -> auto1
 	; Update count_static
 	movf	?auto_main+0,w,c				; auto0 -> w
 	addwf	?auto_main+1,w,c				; w + auto1 -> w
 	addwf	count_static,w,c				; w + count_static -> w
 	movwf	count_static,c					; w -> count_static
-    goto    loop
+	goto	loop
 
-    END     resetVec
+	END		resetVec
